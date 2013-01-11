1c8256b32dd2aba0712bb0799ddac434d1876d09
Dale Wijnand <dale.wijnand@gmail.com>
2013-01-11 09:28:49 +0100

    Version: Add an almost complete implementation (missing latest tag & distance).

---
+++ src/main/scala/Version.scala
@@ -0,0 +1,41 @@
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.Date
+import collection.JavaConverters._
+import org.eclipse.jgit.api.Git
+import org.eclipse.jgit.lib.{IndexDiff, Constants, ObjectId}
+import org.eclipse.jgit.treewalk.FileTreeIterator
+
+object Version {
+  def main(args: Array[String]) {
+    val repoDir = new File("target/test-repo")
+    println("version = " + getVersion(Git.open(repoDir)))
+  }
+
+  def getVersion(git: Git): String = {
+    val repo = git.getRepository
+
+    val headId: ObjectId = repo.resolve(Constants.HEAD)
+
+    val tags = for {
+      tag <- repo.getRefDatabase.getRefs(Constants.R_TAGS).values().asScala
+      if tag.getObjectId == headId
+      name = tag.getName
+      if name(0).isDigit
+    } yield name
+
+    val version = if (tags.isEmpty) {
+      val (latesttag, tagdistance) = getLatestTagWithDistance(git)
+      latesttag + '+' + tagdistance + '-' + headId.name.slice(0, 12)
+    } else tags.last
+
+    val isDirty = new IndexDiff(repo, Constants.HEAD, new FileTreeIterator(repo)).diff()
+
+    if (isDirty) version + "+" + new SimpleDateFormat("yyyyMMdd").format(new Date())
+    else version
+  }
+
+  def getLatestTagWithDistance(git: Git): (String, Int) = {
+    ("abc", 1)
+  }
+}
