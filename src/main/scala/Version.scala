a0f121ebf043573a7aa7da62131e2e7ba85886f2
Dale Wijnand <dale.wijnand@gmail.com>
2013-01-17 11:41:48 +0100

    Version: Implement "latest" tag name & distance logic.

--- src/main/scala/Version.scala
+++ src/main/scala/Version.scala
@@ -3,7 +3,8 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import collection.JavaConverters._
 import org.eclipse.jgit.api.Git
-import org.eclipse.jgit.lib.{IndexDiff, Constants, ObjectId}
+import org.eclipse.jgit.lib.{IndexDiff, Constants}
+import org.eclipse.jgit.revwalk.RevWalk
 import org.eclipse.jgit.treewalk.FileTreeIterator
 
 object Version {
@@ -14,27 +15,36 @@ object Version {
 
   def getVersion(git: Git): String = {
     val repo = git.getRepository
+    val headId = repo.resolve(Constants.HEAD)
 
-    val headId: ObjectId = repo.resolve(Constants.HEAD)
+    val revWalk = new RevWalk(repo)
+    val headCommit = revWalk.parseCommit(headId)
+    revWalk.markStart(headCommit)
 
-    val tags = for {
-      (shortTagName, tag) <- repo.getTags.asScala
-      if tag.getObjectId == headId
-      if shortTagName(0).isDigit
+    val tags = repo.getTags.asScala.toMap
+    val headTags = for {
+      (shortTagName, tag) <- tags
+      tagId = tag.getObjectId
+      revObj = revWalk.parseAny(tagId)
+      peeledRevObj = revWalk.peel(revObj)
+      if peeledRevObj == headId
+    // TODO: Match some regex, see semver
     } yield shortTagName
 
-    val version = if (tags.isEmpty) {
-      val (latesttag, tagdistance) = getLatestTagWithDistance(git)
-      latesttag + '+' + tagdistance + '-' + headId.name.slice(0, 12)
-    } else tags.last
+    val version =
+      if (headTags.isEmpty) {
+        val tagsMap = for {(shortTagName, tag) <- tags} yield (revWalk.parseCommit(tag.getObjectId), shortTagName)
+        val commitSeq = revWalk.asScala.toSeq
+        val commit = commitSeq.find(tagsMap.contains).get
+        val tagName = tagsMap(commit)
+        val tagDistance = commitSeq.indexOf(commit)
+
+        tagName + '+' + tagDistance + '-' + headId.name.slice(0, 12)
+      } else headTags.last
 
     val isDirty = new IndexDiff(repo, Constants.HEAD, new FileTreeIterator(repo)).diff()
 
     if (isDirty) version + "+" + new SimpleDateFormat("yyyyMMdd").format(new Date())
     else version
-  }
-
-  def getLatestTagWithDistance(git: Git): (String, Int) = {
-    ("abc", 1)
   }
 }
