b50c444e3c05fb05d6aeab291e50a58d41684b20
Dale Wijnand <dale.wijnand@gmail.com>
2014-07-01 00:15:16 +0100

    Micro simplify regex.

--- src/main/java/semver4j/SemVer.java
+++ src/main/java/semver4j/SemVer.java
@@ -11,7 +11,7 @@ import static semver4j.Utils.requireNonNegative;
 public final class SemVer {
   private static final Pattern groupingRegex = Pattern.compile('^'
       + "((\\d+)\\.(\\d+)\\.(\\d+))" // version string
-      + "(?:\\-([\\dA-Za-z\\-]+(?:\\.[\\dA-Za-z\\-]+)*))?" // prerelease suffix (optional)
+      + "(?:-([\\dA-Za-z\\-]+(?:\\.[\\dA-Za-z\\-]+)*))?" // prerelease suffix (optional)
       + "(?:\\+([\\dA-Za-z\\-]+(?:\\.[\\dA-Za-z\\-]+)*))?" // build suffix (optional)
       + '$');
 
